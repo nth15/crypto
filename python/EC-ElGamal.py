c9b3f65eef8f4750a1650bfd11bd29602118713e
nth15 <nth15@hi.is>
2019-12-13 22:01:18 +0000

    clean

--- python/EC-ElGamal.py
+++ python/EC-ElGamal.py
@@ -8,7 +8,6 @@ import time
 import random
 import hmac
 
-#------------------------------------
 
 code_strings = {
 	2: '01',
@@ -156,76 +155,6 @@ def encode(val, base, minlen=0):
 
 	return result
 
-#------------------------------------
-#curve configuration
-
-mod = pow(2, 256) - pow(2, 32) - pow(2, 9) - pow(2, 8) - pow(2, 7) - pow(2, 6) - pow(2, 4) - pow(2, 0)
-order = 115792089237316195423570985008687907852837564279074904382605163141518161494337
-
-#curve configuration
-# y^2 = x^3 + a*x + b = y^2 = x^3 + 7
-a = 0
-b = 7
-
-#base point on the curve
-base_point = [55066263022277343669578718895168534326250603453777594175500187360389116729240, 32670510020758816978083085130507043184471273380659243275938904335757337482424]
-
-print("---------------------")
-print("initial configuration")
-print("---------------------")
-print("Curve: y^2 = x^3 + ",a,"*x + ",b, " mod ", mod," , #F(",mod,") = ", order)
-print("Base point: (",base_point[0],", ",base_point[1],")")
-#print("modulo: ", mod)
-#print("order of group: ", order)
-print()
-#------------------------------------
-#symmetric encryption
-
-encryption_begins = time.time()
-
-print("--------------------------------------------------------------")
-print("public key generation")
-
-message = 'hi'
-plaintext = textToInt(message)
-print("message: ",message,". it is numeric matching is ",plaintext)
-
-plain_coordinates = EccCore.applyDoubleAndAddMethod(base_point[0], base_point[1], plaintext, a, b, mod)
-
-print("message is represented as the following point coordinates")
-print("plain coordinates: ",plain_coordinates)
-
-pub = '043e6fbace2ef2ebff56166806ff1d4568ec356edbc0bf97e6fe675f179a017a5af6b0cf80c897ae09a8117392cf8d0f930d494af5b57e2f81518adeeaf6431e1a'
-
-publicKey = decode_pubkey(pub)
-
-print("\npublic key: ",publicKey)
-
-print("--------------------------------------------------------------")
-print("encryption")
-
-randomKey = 28695618543805844332113829720373285210420739438570883203839696518176414791234
-#import random
-#randomKey = random.getrandbits(128)
-
-c1 = EccCore.applyDoubleAndAddMethod(base_point[0], base_point[1], randomKey, a, b, mod)
-
-c2 = EccCore.applyDoubleAndAddMethod(publicKey[0], publicKey[1], randomKey, a, b, mod)
-c2 = EccCore.pointAddition(c2[0], c2[1], plain_coordinates[0], plain_coordinates[1], a, b, mod)
-
-print("\nciphertext")
-print("c1: ", c1)
-print("c2: ", c2)
-
-encryption_ends = time.time()
-
-print("encryption lasts ",encryption_ends-encryption_begins," seconds")
-print("--------------------------------------------------------------")
-#plaintext = c2 - secretKey * c1
-
-decryption_begins = time.time()
-
-
 def encrypt(public_key, message):
 	# Initialize the elliptic curve
 	mod = pow(2, 256) - pow(2, 32) - pow(2, 9) - pow(2, 8) - pow(2, 7) - pow(2, 6) - pow(2, 4) - pow(2, 0)
@@ -264,9 +193,7 @@ def encrypt(public_key, message):
 	c2 = EccCore.pointAddition(c2[0], c2[1], plain_coordinates[0], plain_coordinates[1], a, b, mod)
 	
 def decrypt(priv_key, message):	
-	priv = '69zzh19MGx1eXmwXesokVyzv9QFitn11mZzcCfPhhBAhLMKuesM'
-	#secretKey = 75263518707598184987916378021939673586055614731957507592904438851787542395619
-	secretKey = decode_privkey(priv)
+	secretKey = decode_privkey(priv_key)
 	#secret key times c1
 	dx, dy = EccCore.applyDoubleAndAddMethod(c1[0], c1[1], secretKey, a, b, mod)
 	#-secret key times c1
@@ -294,3 +221,9 @@ def decrypt(priv_key, message):
 	decrytion_end = time.time()
 	print("decryption lasts ",decrytion_end-decrytion_begin," seconds")
 
+def main(fun, key, mess):
+	if fun in ['encrypt', 'e']:
+		encrypt(key, mess)
+	elif fun in ['decrypt', 'd']:
+		decrypt(key, mess)
+	else print('Invalid')
